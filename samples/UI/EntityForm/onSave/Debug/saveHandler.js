d145b50426b0ae816c5b572cad328e25f593dad4
Maros Kolibas <maros.kolibas@resco.net>
2019-01-23 16:41:02 +0100

    SAMPLE: UI EntityForm onSave event method

---
+++ samples/UI/EntityForm/onSave/Debug/saveHandler.js
@@ -0,0 +1,24 @@
+/// <reference path="JSBridge.d.ts"/>
+/** During onSave event suspend save and call asynchronous fetch method. */
+function onSaveHandler() {
+    MobileCRM.UI.EntityForm.onSave(function (entityForm) {
+        var editedAccount = entityForm.entity;
+        // call 'suspendSave' method to allow another asynchronous method execution.
+        var saveHandler = entityForm.suspendSave();
+        // create asynchronous fetch method
+        var fetchEntity = new MobileCRM.FetchXml.Entity("contact");
+        var attribute = new MobileCRM.FetchXml.Attribute("contactid");
+        attribute.aggregate = "count";
+        attribute.alias = "Contact_cnt";
+        fetchEntity.filter = new MobileCRM.FetchXml.Filter();
+        fetchEntity.filter.where("parentcustomerid", "eq", editedAccount.id);
+        var fetch = new MobileCRM.FetchXml.Fetch(fetchEntity);
+        fetch.aggregate = true;
+        fetch.execute("DynamicEntities", function (res) {
+            if (res[0] > 0)
+                saveHandler.resumeSave();
+            else
+                saveHandler.resumeSave("At least one contact is required");
+        }, function (err) { MobileCRM.bridge.alert("Error fetching contact for account.\n\nErr: " + err); }, null);
+    }, true, null);
+}
---
+++ samples/UI/EntityForm/onSave/Debug/saveHandler.ts
@@ -0,0 +1,30 @@
+﻿/// <reference path="JSBridge.d.ts"/>
+
+/** During onSave event suspend save and call asynchronous fetch method. */
+function onSaveHandler() {
+    MobileCRM.UI.EntityForm.onSave((entityForm) => {
+        let editedAccount = entityForm.entity;
+
+        // call 'suspendSave' method to allow another asynchronous method execution.
+        let saveHandler = entityForm.suspendSave();
+
+        // create asynchronous fetch method
+        let fetchEntity = new MobileCRM.FetchXml.Entity("contact");
+        let attribute = new MobileCRM.FetchXml.Attribute("contactid");
+        attribute.aggregate = "count";
+        attribute.alias = "Contact_cnt";
+
+        fetchEntity.filter = new MobileCRM.FetchXml.Filter();
+        fetchEntity.filter.where("parentcustomerid", "eq", editedAccount.id);
+
+        let fetch = new MobileCRM.FetchXml.Fetch(fetchEntity);
+        fetch.aggregate = true;
+        fetch.execute("DynamicEntities", (res) => {
+            if (res[0] > 0)
+                saveHandler.resumeSave();
+            else
+                saveHandler.resumeSave("At least one contact is required");
+        }, (err) => { MobileCRM.bridge.alert("Error fetching contact for account.\n\nErr: " + err); }, null);
+
+    }, true, null);
+}
