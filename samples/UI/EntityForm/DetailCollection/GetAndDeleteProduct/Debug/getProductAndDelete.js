40494505114bc15339ffbfb49d81b37427183918
Maros Kolibas <maros.kolibas@resco.net>
2019-02-13 10:58:35 +0100

    SAMPLE: UI.EntityForm DetailCollection gets sales entity detail and delete it.

---
+++ samples/UI/EntityForm/DetailCollection/GetAndDeleteProduct/Debug/getProductAndDelete.js
@@ -0,0 +1,31 @@
+/// <reference path="JSBridge.d.ts"/>
+/**
+ * Get most expansive product and delete it from sales detail collection
+ * @param deleteById If <c>true</c> deleteById method will be used otherwise we use deleteByIndex method.
+ */
+function getProductAndDelete(deleteById) {
+    MobileCRM.UI.EntityForm.DetailCollection.getAll(function (details) {
+        if (details.length > 0) {
+            /// find most expansive product
+            details.sort(function (d1, d2) { return d2.properties.totalmount - d1.properties.totalamount; });
+            details.sort(function (d1, d2) { return d1.properties.totalmount - d2.properties.totalamount; });
+            var product_1 = details[details.length - 1];
+            if (deleteById) {
+                MobileCRM.UI.EntityForm.DetailCollection.deleteById(product_1.id, function () {
+                    /// success void callback
+                    MobileCRM.bridge.alert("Success to delete most expansive product in the collection [" + product_1.properties.productname + "].");
+                }, function (err) { MobileCRM.bridge.alert("Failed to delete product [" + product_1.properties.productname + "]\n\nErr: " + err); });
+            }
+            else {
+                // delete the product by index
+                MobileCRM.UI.EntityForm.DetailCollection.deleteByIndex(details.length - 1, function () {
+                    /// success void callback
+                    MobileCRM.bridge.alert("Success to delete most expansive product in the collection [" + product_1.properties.productname + "].");
+                }, function (err) { MobileCRM.bridge.alert("Failed to delete product [" + product_1.properties.productname + "]\n\nErr: " + err); });
+            }
+        }
+    }, function (err) {
+        MobileCRM.bridge.alert("Failed to retrieve detail collection.\n\nErr: " + err);
+    }, null);
+}
+//# sourceMappingURL=getProductAndDelete.js.map
---
+++ samples/UI/EntityForm/DetailCollection/GetAndDeleteProduct/Debug/getProductAndDelete.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="JSBridge.d.ts"/>
+
+/**
+ * Get most expansive product and delete it from sales detail collection
+ * @param deleteById If <c>true</c> deleteById method will be used otherwise we use deleteByIndex method.
+ */
+function getProductAndDelete(deleteById: boolean) {
+    MobileCRM.UI.EntityForm.DetailCollection.getAll((details) => {
+        if (details.length > 0) {
+            /// find most expansive product
+            details.sort((d1, d2) => { return d2.properties.totalmount - d1.properties.totalamount; });
+            details.sort((d1, d2) => { return d1.properties.totalmount - d2.properties.totalamount; });
+            let product = details[details.length - 1] as MobileCRM.DynamicEntity;
+
+            if (deleteById) {
+                MobileCRM.UI.EntityForm.DetailCollection.deleteById(product.id, () => {
+                    /// success void callback
+                    MobileCRM.bridge.alert("Success to delete most expansive product in the collection [" + product.properties.productname + "].");
+                }, (err) => { MobileCRM.bridge.alert("Failed to delete product [" + product.properties.productname + "]\n\nErr: " + err); });
+            }
+            else {
+                // delete the product by index
+                MobileCRM.UI.EntityForm.DetailCollection.deleteByIndex(details.length - 1, () => {
+                    /// success void callback
+                    MobileCRM.bridge.alert("Success to delete most expansive product in the collection [" + product.properties.productname + "].");
+                }, (err) => { MobileCRM.bridge.alert("Failed to delete product [" + product.properties.productname + "]\n\nErr: " + err); });
+            }
+        }
+    }, (err) => {
+        MobileCRM.bridge.alert("Failed to retrieve detail collection.\n\nErr: " + err);
+    }, null);
+}
