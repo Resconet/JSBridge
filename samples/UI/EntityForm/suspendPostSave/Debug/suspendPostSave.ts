b00f57dee7c48a6e332b3e9c0c1bad798ef416c9
Lucia Pavlíková <33963381+LuciaPavlikova@users.noreply.github.com>
2019-03-04 14:33:23 +0100

    Update suspendPostSave.ts

--- samples/UI/EntityForm/suspendPostSave/Debug/suspendPostSave.ts
+++ samples/UI/EntityForm/suspendPostSave/Debug/suspendPostSave.ts
@@ -1,30 +1,29 @@
 ﻿/// <reference path="JSBridge.d.ts"/>
 
 /** Create appointment entity after entity was saved. */
-function createAppointmentAfterSave() {
-    MobileCRM.UI.EntityForm.onPostSave((entityForm) => {
-        let entity = entityForm.entity;
-        // post save method to run another asynchronous operation
-        // create new task from newly created entity
-        let postSaveHandler = entityForm.suspendPostSave();
-        let appointment = MobileCRM.DynamicEntity.createNew("appointment");
-        appointment.properties.regardingobjectid = entity;
-        appointment.properties.scheduledstart = new Date().toLocaleDateString();
-        // scheduled end will be set automatically by system as scheduled start + 1 one hour
-        appointment.properties.subject = "Please visit this customer: " + entityForm.entity.primaryName;
-        appointment.properties.statecode = 3; // Set status as scheduled
-
-        appointment.save((err) => {
-            if (err) MobileCRM.bridge.alert("Failed to create appointment.\n\nErr:" + err);
-            // In all cases don't forget to resume post save method.
-            // resume post save handler to finish saving
-            postSaveHandler.resumePostSave();
-        });
-
-    }, true, null);
+function createContactAfterSave() {
+	MobileCRM.UI.EntityForm.onPostSave(
+		function (entityForm) {
+			var entity = entityForm.entity;
+			// post save method to run another asynchronous operation
+			// create new contact from newly created entity
+			var postSaveHandler = entityForm.suspendPostSave();
+			var contact = new MobileCRM.DynamicEntity.createNew("contact");
+			contact.properties.lastname = "Child of " + entity.primaryName;
+			contact.properties.firstname = "I am";
+			contact.properties.parentcustomerid = entity;
+			contact.properties.emailaddress1 = "example@example.com";
+			contact.save(function (err) {
+				if (err)
+					MobileCRM.bridge.alert("Failed to create contact.\n\nErr:" + err);
+				// In all cases don't forget to resume post save method.
+				// resume post save handler to finish saving
+				postSaveHandler.resumePostSave();
+			});
+		}, true, MobileCRM.bridge.alert);
 }
 
 /** Register event during on-load event of entity form. */
-window.onload = () => {
-    createAppointmentAfterSave();
+window.onload = function () {
+	createContactAfterSave();
 }
