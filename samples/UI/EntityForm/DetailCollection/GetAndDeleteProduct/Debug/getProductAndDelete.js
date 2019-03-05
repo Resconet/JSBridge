cd2e77b0600fd91e33ee3472cee476d933d50c6c
Lucia Pavlíková <33963381+LuciaPavlikova@users.noreply.github.com>
2019-03-05 15:44:57 +0100

    Update getProductAndDelete.js

--- samples/UI/EntityForm/DetailCollection/GetAndDeleteProduct/Debug/getProductAndDelete.js
+++ samples/UI/EntityForm/DetailCollection/GetAndDeleteProduct/Debug/getProductAndDelete.js
@@ -7,8 +7,7 @@ function getProductAndDelete(deleteById) {
     MobileCRM.UI.EntityForm.DetailCollection.getAll(function (details) {
         if (details.length > 0) {
             /// find most expansive product
-            details.sort(function (d1, d2) { return d2.properties.totalmount - d1.properties.totalamount; });
-            details.sort(function (d1, d2) { return d1.properties.totalmount - d2.properties.totalamount; });
+            details.sort(function (d1, d2) { return (d1.properties.priceperunit > d2.properties.priceperunit) ? 1 : ((d2.properties.priceperunit > d1.properties.priceperunit) ? -1 : 0); });
             var product_1 = details[details.length - 1];
             if (deleteById) {
                 MobileCRM.UI.EntityForm.DetailCollection.deleteById(product_1.id, function () {
@@ -17,11 +16,11 @@ function getProductAndDelete(deleteById) {
                 }, function (err) { MobileCRM.bridge.alert("Failed to delete product [" + product_1.properties.productname + "]\n\nErr: " + err); });
             }
             else {
-                // delete the product by index
-                MobileCRM.UI.EntityForm.DetailCollection.deleteByIndex(details.length - 1, function () {
-                    /// success void callback
-                    MobileCRM.bridge.alert("Success to delete most expansive product in the collection [" + product_1.properties.productname + "].");
-                }, function (err) { MobileCRM.bridge.alert("Failed to delete product [" + product_1.properties.productname + "]\n\nErr: " + err); });
+                // delete the last product by index
+				MobileCRM.UI.EntityForm.DetailCollection.deleteByIndex(details.length - 1, function () {
+					/// success void callback
+					MobileCRM.bridge.alert("Success to delete last product in the collection.");
+				}, function (err) { MobileCRM.bridge.alert("Failed to delete last product in the collection." + err); });
             }
         }
     }, function (err) {
