192b32d6ebcdada51302beadf06659deb836992e
Maros Kolibas <maros.kolibas@resco.net>
2019-04-10 11:51:48 +0200

    SAMPLES: EntityList onSave event handler with validation.

---
+++ samples/UI/EntityList/OnSave/Debug/onSave.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="JSBridge.d.ts"/>
+
+/**
+ * Validate 'Text-Edit' kind property 'fax' on edited entities during on save event.
+ * @param register if 'true' handler is bound.
+ */
+function validateFaxPropertyOnSave(register: boolean) {
+	MobileCRM.UI.EntityList.onSave(entityList => {
+		let editedEntities = entityList.context.entities;		let saveHandler = entityList.suspendSave();		let errMsg = "";		for (var i in editedEntities) {			var entity = editedEntities[i];			if (entity.properties.fax.length < 3) {				errMsg += "Record : [" + entity.primaryName + "] has short fax\n";			}		}		saveHandler.resumeSave(errMsg);
+	}, register, null);
+}
+/** Register onSave event handler when page is loaded. */
+window.onload = () => {
+	validateFaxPropertyOnSave(true);
+}
