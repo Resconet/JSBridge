8de7b5b89bb4332ffa357fe192bf0ba6dbc0b87c
maros316 <maros.kolibas@gmail.com>
2020-03-30 10:52:05 +0200

    Upload Integration folder with PowerBi source files.

---
+++ Integration/PowerBi/PowerBiOAuthSettings.js
@@ -0,0 +1,10 @@
+/** PowerBi OAuth settings */
+var PowerBiOAuthSettings = {
+	client_id: "f0169aa6-6d11-406b-b408-334d53ff687b",
+	client_secrete: "sA7Tz0t_BYR/ZiK_SEIfPf9Zj5HhRPm*",
+	dashboardId: "f858d56b-7c59-4805-90b0-fadc76712fa8", // GOT from URL
+	reportId: 'e49beb7b-c418-41cc-93f2-496c950d0a1b',
+	workspaceGroupId: 'a4b84057-eba2-49bc-a4f6-b22eef509913',
+	redirectUrl: 'https://www.resco.net/oauth.html',
+	resourceUrl: 'https://analysis.windows.net/powerbi/api'
+};
