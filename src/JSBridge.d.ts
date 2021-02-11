80e8eb547e38e8e7ef7bc44904bfefba037188ba
Maros316 <maros.kolibas@gmail.com>
2021-02-11 15:54:51 +0100

    Update JSBridge.d.ts
    Allow to execute loadFrom and loadFromMultiple action

--- src/JSBridge.d.ts
+++ src/JSBridge.d.ts
@@ -1107,6 +1107,8 @@ declare module MobileCRM.Services {
         capturePhoto(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
         selectPhoto(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
         selectMultiplePhotos(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
+	loadFrom(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
+	loadFromMultiple(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
         selectFile(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
         recordAudio(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
         recordVideo(callback: (fileInfo: FileInfo) => void, errorCallback?: (error: string) => void, scope?: any);
